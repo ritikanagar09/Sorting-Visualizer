b20e7affe5f11a46ec4a0bf14f4faf779bf1e810
Ritika Nagar <ritika20112@iiitd.ac.in>
2022-06-22 16:04:06 +0530

    Co-authored-by: shobha kumari  <SHOBHA-0502@users.noreply.github.com>

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -15,19 +15,27 @@ function Bar({index,length,color}){
 
     const inputStyle={
         position: 'relative',
-        top: Math.floor(length/2)-10,
+        top: Math.floor(length/2)-12,
         width:length,
-        left: -Math.floor(length/2)+10,
+        left: -Math.floor(length/2)+13,
         border:'none',
+        background : 'none',
     }
-    const front_bottom={
+    const bottom={
         transform: `translateY(${200-length}px) rotateX(-90deg)`,
         backgroundColor: `${colors[color][0]}`,
         boxShadow:`5px 5px 50px 5px ${colors[color][1]}`,// we have used this brackets for dynamically generated strings 
         transition:'0.3s'// this is a normal strings ..so no need to send it like with $ sign
     }
+    // const front_back ={
+    //     transform: `translateY(${200-length}px) rotateX(-90deg)`,
+    //     backgroundColor: `${colors[color][0]}`,
+    //     boxShadow:`5px 5px 50px 5px ${colors[color][1]}`,// we have used this brackets for dynamically generated strings 
+    //     transition:'0.3s'// this is a normal strings ..so no need to send it like with $ sign
 
-    const right_left={
+    // }
+
+    const front_back_right_left={
         height: `${length}px`,
         transform:`translateY(${200-length}px)`,
         backgroundColor:`${colors[color][0]}`,
@@ -51,23 +59,19 @@ function Bar({index,length,color}){
         setLen(parseInt(e.target.value))
         
     }
-
-    
-    
-
     return(
         <>
             <div className = "bar" style={barStyle}>
                 <div className="side top"></div>
-                <div className="side bottom" style={front_bottom}></div>
+                <div className="side bottom" style={bottom}></div>
                 <div className="side right" >
-                    <div className="color-bar right-color-bar" style={right_left}></div>
+                    <div className="color-bar right-color-bar" style={front_back_right_left}></div>
                 </div>
                 <div className="side left" >
-                    <div className="color-bar left-color-bar" style={right_left}></div>
+                    <div className="color-bar left-color-bar" style={front_back_right_left}></div>
                 </div>
                 <div className="side front" >
-                    <div className="color-bar front-color-bar" style={front_bottom}></div>
+                    <div className="color-bar front-color-bar" style={front_back_right_left}></div>
                     <input type="number" 
                     length={length} 
                     style= {inputStyle} 
@@ -77,7 +81,7 @@ function Bar({index,length,color}){
                     />
                 </div>
                 <div className="side back">
-                    <div className="color-bar back-color-bar" style={front_bottom}></div>
+                    <div className="color-bar back-color-bar" style={front_back_right_left}></div>
                 </div>
             </div>
         </>
