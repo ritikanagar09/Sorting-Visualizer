d2700d3ce5e80c8e7cd1f0ab8e0db9d3dfa21063
Ritika Nagar <ritika20112@iiitd.ac.in>
2022-06-22 15:26:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React,{Component} from 'react';
+import Bar from './components/Bar';
+import './App.css';
+// props--> properties that are passed in
+class App extends Component{
+    state={// state is an object
+        array: [],
+        arraySteps: [],
+        colorKey:[],
+        colrSteps:[],
+        currSteps:0,
+        count:10,
+        delay:100,
+        algorithm:'',
+        timeouts: [],
+    
+    };
+
+
+// function for calling the genrate randam array
+    componentDidMount(){
+        this.generateArray();
+;    }
+
+    // function to generate an random array
+    generateRandomNum=(min, max)=>{
+        return Math.floor(Math.random() * (max-min)+min); //more than the minimum value and less than the max 
+    }
+    generateArray=() =>{
+        const count=this.state.count;
+        let temp =[];// empty array
+
+        for(let i=0 ;i<count;i++){
+  
+            
+            temp.push(this.generateRandomNum(50,200));
+        }
+    //   console.log(temp)
+    this.setState({
+        array:temp,
+        arraySteps: [temp]
+    });
+    };
+    render(){
+        let bars = this.state.array.map((value,index)=>(
+           
+          <Bar 
+           key = {index} 
+           index = {index}
+           length = {value}
+           color = {0} 
+           />
+        )); 
+
+        // h1 cannot include the div/components
+        return (
+            <div className='app'>
+                <div className="frame">
+                    <div className="barsDiv container card "> {bars}</div>
+                <div className="control-panel"></div>
+                <div className="pannel"></div>
+                </div>
+            </div>
+      );
+    }
+}
+
+export default App;
---
+++ src/components/Bar.js
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import './Bar.css';
+function Bar({index,length,color}){
+
+    const [len, setLen]=useState(length)
+    const colors=[
+        ['rgba(61,90,241,0.5)','rgba(61,90,241,0.2)'],
+        ['rgba(255,48,79,1)','rgba(255,48,79,0.5)'],
+        ['rgba(131,232,90,0.5)','rgba(131,232,90,0.2)'],
+
+    ];
+    const barStyle = {
+        height : length,
+    };
+
+    const inputStyle={
+        position: 'relative',
+        top: Math.floor(length/2)-10,
+        width:length,
+        left: -Math.floor(length/2)+10,
+        border:'none',
+    }
+    const front_bottom={
+        transform: `translateY(${200-length}px) rotateX(-90deg)`,
+        backgroundColor: `${colors[color][0]}`,
+        boxShadow:`5px 5px 50px 5px ${colors[color][1]}`,// we have used this brackets for dynamically generated strings 
+        transition:'0.3s'// this is a normal strings ..so no need to send it like with $ sign
+    }
+
+    const right_left={
+        height: `${length}px`,
+        transform:`translateY(${200-length}px)`,
+        backgroundColor:`${colors[color][0]}`,
+        boxShadow:`5px 5px 50px 5px ${colors[color][1]}`,
+        transition:'0.3s'
+    }
+
+    const handleChange=(e)=>{
+        let val= e.target.value;
+        if(val===''){
+            setLen(0);
+        }else{
+            val=parseInt(val);
+            if(val>200){
+                setLen(200);
+            }else{
+                setLen(val);
+            }
+            
+        }
+        setLen(parseInt(e.target.value))
+        
+    }
+
+    
+    
+
+    return(
+        <>
+            <div className = "bar" style={barStyle}>
+                <div className="side top"></div>
+                <div className="side bottom" style={front_bottom}></div>
+                <div className="side right" >
+                    <div className="color-bar right-color-bar" style={right_left}></div>
+                </div>
+                <div className="side left" >
+                    <div className="color-bar left-color-bar" style={right_left}></div>
+                </div>
+                <div className="side front" >
+                    <div className="color-bar front-color-bar" style={front_bottom}></div>
+                    <input type="number" 
+                    length={length} 
+                    style= {inputStyle} 
+                    className="input"
+                    value={len} 
+                    onChange={handleChange}
+                    />
+                </div>
+                <div className="side back">
+                    <div className="color-bar back-color-bar" style={front_bottom}></div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Bar;
